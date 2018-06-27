bd23689f9378d8b6ce2cafeed4236f014d9b1a18
Karen Flores <karen.ag.flores@gmail.com>
2018-06-27 22:35:15 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,11 +12,8 @@ class Anagram
     #anagram_list_words = anagram_list.split(", ")
     anagram_array = []
     #anagram_list.each{|letters| letters.split("").sort}
-    if anagram_list.find_all {|new_word| new_word.split("").sort == word_letters}
+    anagram_list.find_all {|new_word| new_word.split("").sort == word_letters}
 
-    else
-      anagram_array
-    end
 
   end
 end
