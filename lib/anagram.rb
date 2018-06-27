4dbfc637ce90918845f92fd1f493dbd7e2c952d2
Karen Flores <karen.ag.flores@gmail.com>
2018-06-27 22:29:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word= word
+  end
+
+  def match(anagram_list)
+    word_letters = @word.split("").sort
+    #anagram_list_words = anagram_list.split(", ")
+    anagram_array = []
+    #anagram_list.each{|letters| letters.split("").sort}
+    if anagram_list.find_all {|new_word| new_word.split("").sort == word_letters
+      
+    else
+      anagram_array
+    end
+
+  end
+end
